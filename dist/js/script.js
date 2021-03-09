516e6c1e13b49aae017e73d319054c0f60054ae5
Valerii Stolbov <61787196+stolbovvv@users.noreply.github.com>
2021-03-09 17:50:20 +0500

    clear project 2021-03-09

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,33 +1,19 @@
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
 
-    // const menu = document.querySelector('.menu'),
-    //     menuItem = document.querySelectorAll('.menu_item'),
-    //     burger = document.querySelector('.burger');
+// const menu = document.querySelector('.menu'),
+//     menuItem = document.querySelectorAll('.menu_item'),
+//     burger = document.querySelector('.burger');
 
-    // burger.addEventListener('click', () => {
-    //     burger.classList.toggle('burger_active');
-    //     menu.classList.toggle('menu_active');
-    // });
+// burger.addEventListener('click', () => {
+//     burger.classList.toggle('burger_active');
+//     menu.classList.toggle('menu_active');
+// });
 
-    // menuItem.forEach(item => {
-    //     item.addEventListener('click', () => {
-    //         burger.classList.toggle('burger_active');
-    //         menu.classList.toggle('menu_active');
-    //     })
-    // })
+// menuItem.forEach(item => {
+//     item.addEventListener('click', () => {
+//         burger.classList.toggle('burger_active');
+//         menu.classList.toggle('menu_active');
+//     })
+// })
 
-    const selects = document.querySelectorAll('.select');
-    selects.forEach(item => {
-        item.addEventListener('click', () => item.classList.toggle('select_active'), true)
-        console.log(item);
-    })
-
-    const secletLists = document.querySelectorAll('.select__list-item');
-    secletLists.forEach(item => {
-        item.addEventListener('click', () => {
-            item.parentElement.parentElement.firstElementChild.value = item.innerHTML;
-            item.parentElement.parentElement.parentElement.classList.remove('select_active');
-        })
-    })
-
-})
+// })
