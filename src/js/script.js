9028cfd6d25397296df9e9d16f57191f24e78f0b
Valerii Stolbov <61787196+stolbovvv@users.noreply.github.com>
2021-03-10 19:33:35 +0500

    add header 2021-03-10

--- src/js/script.js
+++ src/js/script.js
@@ -1,19 +1,21 @@
-// window.addEventListener('DOMContentLoaded', () => {
+window.addEventListener('DOMContentLoaded', () => {
 
-// const menu = document.querySelector('.menu'),
-//     menuItem = document.querySelectorAll('.menu_item'),
-//     burger = document.querySelector('.burger');
+    const menuMobile = document.querySelector('.menu__list_mobile');
+    const menuItem = document.querySelector('.menu__list-item-link');
+    const burger = document.querySelector('.menu__burger');
 
-// burger.addEventListener('click', () => {
-//     burger.classList.toggle('burger_active');
-//     menu.classList.toggle('menu_active');
-// });
+    burger.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        menuMobile.classList.toggle('active');
+    });
 
-// menuItem.forEach(item => {
-//     item.addEventListener('click', () => {
-//         burger.classList.toggle('burger_active');
-//         menu.classList.toggle('menu_active');
-//     })
-// })
+    // menuItem.forEach(item => {
+    //     console.log(item)
+    //     item.addEventListener('click', () => {
+    //         burger.classList.toggle('burger_active');
+    //         menuMobile.classList.toggle('menu_active');
+    //      })
+    // })
 
-// })
+    console.log(menuItem)
+})
