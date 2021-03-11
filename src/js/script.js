ee291251f76086c24c6b5df54f56e40e0259d9cd
Valerii Stolbov <61787196+stolbovvv@users.noreply.github.com>
2021-03-11 18:35:52 +0500

    create and adaptiv section FEATURES, TESTIMONIALS, CONTENT. Fix bug on menu script 2021-03-11

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
     const menuMobile = document.querySelector('.menu__list_mobile');
-    const menuItem = document.querySelector('.menu__list-item-link');
+    const menuItem = document.querySelectorAll('.menu__list-item-link');
     const burger = document.querySelector('.menu__burger');
 
     burger.addEventListener('click', () => {
@@ -9,13 +9,10 @@ window.addEventListener('DOMContentLoaded', () => {
         menuMobile.classList.toggle('active');
     });
 
-    // menuItem.forEach(item => {
-    //     console.log(item)
-    //     item.addEventListener('click', () => {
-    //         burger.classList.toggle('burger_active');
-    //         menuMobile.classList.toggle('menu_active');
-    //      })
-    // })
-
-    console.log(menuItem)
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            burger.classList.toggle('active');
+            menuMobile.classList.toggle('active');
+        })
+    })
 })
